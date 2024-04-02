3f991d652d2e0384faae381fc34891e7a2f2c1e5
Fajri135 <corex135@gmail.com>
2024-04-02 12:27:19 +0700

    Menambahkan getline untuk membaca input dari pengguna dan menyimpannya dalam atribut nim dari variabel mhs.nim

--- StrukturD_3/StrukturD_3.cpp
+++ StrukturD_3/StrukturD_3.cpp
@@ -2,7 +2,20 @@
 #include <string>
 using namespace std;
 
-int main()
-{
-    ;
+struct DetailAlamat {
+    string desa;
+    string kota;
+};
+
+struct Mahasiswa {
+    string nim;
+    string nama;
+    DetailAlamat alamat;
+    int umur;
+};
+
+int main() {
+    Mahasiswa mhs;
+    cout << "Nomor Mahasiswa : ";
+    getline(cin, mhs.nim);
 }
