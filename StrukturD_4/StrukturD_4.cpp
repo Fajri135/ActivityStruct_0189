ad947dabbd52446333047f6c3006f9303123351a
Fajri135 <corex135@gmail.com>
2024-04-02 12:38:03 +0700

    Menambahkan variabel i pada data mhs

--- StrukturD_4/StrukturD_4.cpp
+++ StrukturD_4/StrukturD_4.cpp
@@ -17,6 +17,21 @@ struct Mahasiswa {
 int main() {
     Mahasiswa mhs[3];
     for (int i = 0; i < 3; i++) {
+        cout << "Data Ke-" << (i + 1) << ":" << endl;
+        cout << "Nomor Mahasiswa : ";
+        getline(cin, mhs[i].nim);
+        cout << "Nama Mahasiswa : ";
+        getline(cin, mhs[i].nama);
+        cout << "Alamat Mahasiswa : " << endl;
+        cout << "\t Nama Desa : ";
+        getline(cin, mhs[i].alamat.desa);
+        cout << "\t Nama Kota : ";
+        getline(cin, mhs[i].alamat.kota);
+        cout << "Umur Mahasiswa : ";
+        cin >> mhs[i].umur;
+        cin.ignore(); 
+        cout << endl;
+    }
 
 }
 
